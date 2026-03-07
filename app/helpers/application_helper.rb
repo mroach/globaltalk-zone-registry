3ad3aa7d43cb5ec81cf1901dceef52db3e1caed9
mroach <git@c.mroach.com>
2026-03-07 23:39:38 +0100

    change: filter-out HTML in markdown rendering

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,11 +2,19 @@ module ApplicationHelper
   def render_markdown(str)
     return nil if str.blank?
 
+    renderer = Redcarpet::Render::HTML.new(filter_html: true)
+    options = {
+      autolink: true,
+      fenced_code_blocks: true,
+      highlight: true,
+      no_intra_emphasis: true,
+      strikethrough: true,
+      tables: true,
+      underline: true
+    }
+
     tag.div(class: "markdown") do
-      Redcarpet::Markdown
-        .new(Redcarpet::Render::HTML, autolink: true, tables: true)
-        .render(str)
-        .html_safe
+      Redcarpet::Markdown.new(renderer, options).render(str).html_safe
     end
   end
 end
