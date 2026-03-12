0d3475b7b5b23418cb6ae61f3b81641edeb66238
mroach <git@c.mroach.com>
2026-03-12 22:43:22 +0100

    change: don't nilify endpoints
    We may set these manually for some zones

--- app/modules/sheet_ingestor/update_job.rb
+++ app/modules/sheet_ingestor/update_job.rb
@@ -4,12 +4,13 @@ module SheetIngestor
       zones = SheetIngestor::GoogleSheetReader.new.get_zones
 
       zones.each do |zd|
+        # Don't null-out network ranges that we may have set manually
         ExternalZone.upsert({
           source: "globaltalk_original",
           name: zd.name,
-          network_ranges: zd.network_ranges,
+          network_ranges: zd.network_ranges.empty? ? nil : zd.network_ranges,
           public_endpoint: zd.endpoint
-        }, on_duplicate: :update, unique_by: :name)
+        }.compact, on_duplicate: :update, unique_by: :name)
       end
 
       # Wait for the zone checks to be probably done
